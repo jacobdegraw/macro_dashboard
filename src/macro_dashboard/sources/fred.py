5b26a1b69f5a4de01ad5c193b64cd4054d8a948a
Jake DeGraw <jacobdegraw1040@gmail.com>
2026-01-30 17:48:08 -0800

    Started setting up FRED interface

---
+++ src/macro_dashboard/sources/fred.py
@@ -0,0 +1,18 @@
+import requests
+
+class Fred:
+
+    def pull_series_metadata(series_id):
+        pass
+
+    def pull_series_observations(series_id):
+        pass
+
+    def pull_series_release(series_id):
+        pass
+
+    def pull_releases():
+        pass
+
+    def pull_release_dates(release_id):
+        pass
